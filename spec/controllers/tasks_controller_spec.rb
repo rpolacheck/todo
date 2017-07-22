c2e3a2a4b09403ff6408391c4a574390813c9913
Robert Polacheck <rpolacheck@gmail.com>
2017-07-22 19:48:02 +0000

    Added tasks update

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,12 +3,17 @@ require 'rails_helper'
 RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
     it "should list the tasks in the database" do
-        task1 = FactoryGirl.create(:task)
-        task2 = FactoryGirl.create(:task)
-        get :index
-        expect(response).to have_http_status :success
-        response_value = ActiveSupport::JSON.decode(@response.body)
-        expect(response_value.count).to eq(2)
+      task1 = FactoryGirl.create(:task)
+      task2 = FactoryGirl.create(:task)
+      task1.update_attributes(title: "Something else")
+      get :index
+      expect(response).to have_http_status :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value.count).to eq(2)
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
